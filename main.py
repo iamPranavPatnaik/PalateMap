6c9bd154967548960307ae925a60573a743b0f50
Praneeth Suryadevara <spraneeth42@gmail.com>
2024-07-24 16:20:36 -0400

    firebase authentication

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from gui import GUI
 from evaluateFlavor import FlavorEvaluator
 from dishSearch import DishSearcher
 from tkinter import messagebox
+from loginPage import LoginPage
+from registrationPage import RegistrationPage
 
 class Main:
-    def __init__(self):
+    def __init__(self, user_id):
+        self.user_id = user_id
         self.searcher = DishSearcher()
         # Pass the submitDish method as a callback
         self.gui = GUI(self.submitDish)
@@ -20,8 +23,27 @@ class Main:
         else:
             messagebox.showwarning("Input Error", "Please enter a dish name.")
 
-def startPalateMap():
-    main = Main() 
+def startPalateMap(user_id):
+    main = Main(user_id) 
     main.gui.run()  # Run the GUI loop
 
-startPalateMap()
+def onLoginSuccess(user_id):
+    # Destroy login window and start the main application
+    login_page.root.destroy()
+    startPalateMap(user_id)
+
+def showRegistrationPage():
+    def on_register_success():
+        register_page.root.destroy()
+        startLoginPage()
+
+    register_page = RegistrationPage(on_register_success)
+    register_page.run()
+
+def startLoginPage():
+    global login_page
+    login_page = LoginPage(onLoginSuccess, showRegistrationPage)
+    login_page.run()
+
+if __name__ == "__main__":
+    startLoginPage() # Basically just call it when its started
