912e47595aa48370b0bf9ca1672f93863196ac3e
Pranav Patnaik <137661401+iamPranavPatnaik@users.noreply.github.com>
2024-07-24 02:06:01 -0400

    Add readMenu.py

--- ocrMenu.py
+++ ocrMenu.py
@@ -1,58 +1,59 @@
 import requests
+import os
 import json
+from dotenv import load_dotenv
+import requests
+import json
+load_dotenv()
 
-def ocr_space_file(filename, overlay=False, api_key='K87609404488957', language='eng'):
-    """ OCR.space API request with local file.
-        Python3.5 - not tested on 2.7
-    :param filename: Your file path & name.
-    :param overlay: Is OCR.space overlay required in your response.
-                    Defaults to False.
-    :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
-    :param language: Language code to be used in OCR.
-                    List of available language codes can be found on https://ocr.space/OCRAPI
-                    Defaults to 'en'.
-    :return: Result in JSON format.
-    """
+class OCRMenu:
+    def __init__(self, api_key=None):
+        # Load CSV file
+        api_key = os.getenv("OCR_API_KEY")
+        self.api_key = api_key
 
-    payload = {'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
-    return r.content.decode()
+    def ocr_space_file(self, filename, overlay=False, language='eng'):
+        """ OCR.space API request with local file.
+        :param filename: Your file path & name.
+        :param overlay: Is OCR.space overlay required in your response. Defaults to False.
+        :param language: Language code to be used in OCR. Defaults to 'eng'.
+        :return: Result in JSON format.
+        """
+        payload = {
+            'isOverlayRequired': overlay,
+            'apikey': self.api_key,
+            'language': language,
+        }
+        with open(filename, 'rb') as f:
+            r = requests.post('https://api.ocr.space/parse/image', files={filename: f}, data=payload)
+        return r.content.decode()
 
+    def ocr_space_url(self, url, overlay=False, language='eng'):
+        """ OCR.space API request with remote file.
+        :param url: Image url.
+        :param overlay: Is OCR.space overlay required in your response. Defaults to False.
+        :param language: Language code to be used in OCR. Defaults to 'eng'.
+        :return: Result in JSON format.
+        """
+        payload = {
+            'url': url,
+            'isOverlayRequired': overlay,
+            'apikey': self.api_key,
+            'language': language,
+        }
+        r = requests.post('https://api.ocr.space/parse/image', data=payload)
+        return r.content.decode()
 
-def ocr_space_url(url, overlay=False, api_key='K87609404488957', language='eng'):
-    """ OCR.space API request with remote file.
-        Python3.5 - not tested on 2.7
-    :param url: Image url.
-    :param overlay: Is OCR.space overlay required in your response.
-                    Defaults to False.
-    :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
-    :param language: Language code to be used in OCR.
-                    List of available language codes can be found on https://ocr.space/OCRAPI
-                    Defaults to 'en'.
-    :return: Result in JSON format.
-    """
+    def parse_menu_file(self, filename):
+        """ Parse the menu file and return the parsed text.
+        :param filename: Your file path & name.
+        :return: Parsed text from the image.
+        """
+        response = self.ocr_space_file(filename, language='eng')
+        response_json = json.loads(response)
+        return response_json["ParsedResults"][0]["ParsedText"]
 
-    payload = {'url': url,
-               'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload,
-                      )
-    return r.content.decode()
-
-menu_file = ocr_space_file(filename=r'C:\Users\prana\Pictures\Screenshots\Screenshot 2024-07-24 001555.png', language='eng')
-menu_file = json.loads(menu_file)
-menu = menu_file["ParsedResults"][0]["ParsedText"]
-
-print(menu)
+if __name__ == "__main__":
+    ocr = OCRMenu(api_key='K87609404488957')
+    menu_text = ocr.parse_menu_file(filename=r"C:\Users\prana\Downloads\menuexample.png")
+    print(menu_text)
